0d840576ecf0458194bd5928db782c3e50291ed4
shahriar021 <shahriarc19@gmail.com>
2026-03-11 13:11:10 +0600

    fix:remove useeffect

--- src/hooks/useWatchLater.ts
+++ src/hooks/useWatchLater.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
 export const useWatchLater =()=>{
     const [watchLaterIds, setWatchLaterIds] = useState<string[]>(() => {
